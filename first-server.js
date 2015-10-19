70b1e8759fbea3202d8ce77b48162ba20a52a368
A00221443 <a00221443@student.ait.ie>
2015-10-19 14:28:16 +0100

    Update first-server.js

--- first-server.js
+++ first-server.js
@@ -27,7 +27,7 @@ var consolere = require('console-remote-client').connect('console.re','80','A002
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('Yes! It worked!! Path Hit: ' + server_ip_address + ":" + server_port + "/" + request.url);
+    response.end('Help Im trapped inside a computer! Path Hit: ' + server_ip_address + ":" + server_port + "/" + request.url);
     console.re.log("My server ip address is: " + server_ip_address + ":" + server_port);
 }
 //What is a request?
